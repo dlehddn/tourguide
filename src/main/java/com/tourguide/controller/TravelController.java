8c7e4d7e77acad6f0ad88ae1110de08f35100b42
woo <ldw28517@naver.com>
2023-06-03 19:13:25 +0900

    최종

--- src/main/java/com/tourguide/controller/TravelController.java
+++ src/main/java/com/tourguide/controller/TravelController.java
@@ -1,5 +1,6 @@
 package com.tourguide.controller;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -58,7 +59,7 @@ public class TravelController {
         // ChatGPT API 호출 코드
         String mbti_type = memberService.getMmbti();
         String prompt = "내 MBTI는\n" + mbti_type + "이고\n" + region + "에서\n" + days + "일동안\n" + "여행할건데 "
-                        + "나의 MBTI성향에 대해 간단하게 설명해주고, 나한테 어울리는 여행코스를 맛집도 포함해서 날짜별로 자세한 설명과 함께 추천해줘.";
+                        + "나의 MBTI성향에 대해 간단하게 설명해주고, 내 성향에 어울이는 여행코스를 추천해줬으면 해. 각각의 여행 날짜별로 아침, 점심, 저녁식사는 어디서 뭘 먹고 중간중간 놀거리나 관광지는 어떤걸 즐기면 좋을지 간단한 설명과 함께 추천해줘.";
         String response = callChatGPTAPI(prompt);
         String recommendation = response;
         return recommendation;
@@ -67,36 +68,42 @@ public class TravelController {
         try {
             MediaType mediaType = MediaType.parse("application/json");
             OkHttpClient client = new OkHttpClient.Builder()
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .writeTimeout(30, TimeUnit.SECONDS) // http 최대 요청시간 정의
+            .connectTimeout(300, TimeUnit.SECONDS)
+            .readTimeout(300, TimeUnit.SECONDS)
+            .writeTimeout(300, TimeUnit.SECONDS) // http 최대 요청시간 정의
             .build(); 
 
-            String apikey = "api_key 입력";
+            String apikey = "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";
 
             JSONObject jsonObject = new JSONObject();
+            JSONObject message = new JSONObject();
+            JSONArray messageList = new JSONArray();
+            message.put("role", "user");
+            message.put("content", prompt);
+            messageList.put(message);
+
             jsonObject.put("model", "text-davinci-003");
             jsonObject.put("prompt", prompt);
+            /* jsonObject.put("messages", messageList); */
             jsonObject.put("max_tokens",   1500);
             jsonObject.put("temperature", 0.7);
             jsonObject.put("top_p", 1);
             jsonObject.put("frequency_penalty", 0);
             jsonObject.put("presence_penalty", 0);
             jsonObject.put("stream", false);
-            String json = jsonObject.toString();
+            String json = jsonObject.toString(); 
+
             RequestBody body = RequestBody.create(json, mediaType);
             Request request = new Request.Builder()
                     .url("https://api.openai.com/v1/completions")
                     .post(body)
                     .addHeader("Content-Type", "application/json")
-                    .addHeader("Authorization", "Bearer " + apikey)
+                    .addHeader("Authorization", "Bearer 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")
                     .build();
 
             Response response = client.newCall(request).execute();
-            
-            
             ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode rootNode = objectMapper.readTree(response.body().byteStream());
+            JsonNode rootNode = objectMapper.readTree(response.body().string());
             String response_text = rootNode.get("choices").get(0).get("text").asText();
             String response_final = response_text.replaceAll("\n", "<br/>");
 
